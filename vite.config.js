bdfeb48a87b3ac2d395063ccadffc368736c4b8c
xhiminjo69 <xh.leka@gmail.com>
2025-11-03 15:55:30 +0100

    Remove unused Emotion configuration

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/beluga-restaurant/' : '/',
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin']
-      }
-    })
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
